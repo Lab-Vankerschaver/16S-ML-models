d41b6ebcc8a54a5f33ad1cf1bde1c1ab137745f1
Robbysseus <87700333+Robbysseus@users.noreply.github.com>
2022-05-11 19:54:04 +0900

    Add files via upload

--- preprocessing_scripts/preprocessing_CNN.py
+++ preprocessing_scripts/preprocessing_CNN.py
@@ -1,37 +1,34 @@
 # LOADING PACKAGES
-
 import numpy as np
 import pandas as pd
-from itertools import product
-
 print('Packages loaded!')
 
-# LOADING THE NON-AUGMENTED AND AUGMENTED DATASETS
+####################################################################################################
 
+# LOADING THE NON-AUGMENTED AND AUGMENTED DATASETS
 train_na = pd.read_csv('df_train_0.csv')
 val_na = pd.read_csv('df_val_0.csv')
 test_na = pd.read_csv('df_test_0.csv')
-
+# ------------------------------------------------
 train_a = pd.read_csv('df_train_1.csv')
 val_a = pd.read_csv('df_val_1.csv')
-
+# ------------------------------------------------
 print('Datasets loaded!')
 
-# Generating k-mers
-# For use in the Convolutional Neural Networks (CNN), the sequences are processed into a frequency table of k-mers.
+####################################################################################################
 
+from itertools import product
 # define all possible k-mers
 alphabet = "AGTCN"
-three_mers = [''.join(chars) for chars in product(*(3*(alphabet,)))]
+tri_mers = [''.join(chars) for chars in product(*(3*(alphabet,)))]
 six_mers = [''.join(chars) for chars in product(*(6*(alphabet,)))]
 
 def one_hot_k(sequence, kmers):
-    # k-mer length
     k = len(kmers[0])
-    # initialise k-mer dictionary with all possible k-mers as keys and count (0) as values
+    # define a counter dictionary
     kmer_dict = dict.fromkeys(kmers, 0)
 
-    # standardise the sequence
+    # standardize the sequence
     # by replacing U with T and all ambiguous bases with N
     sequence = sequence.replace('U', 'T').replace('Y', 'N').replace('R', 'N').replace('W', 'N').replace('S', 'N').replace('K', 'N').replace('M', 'N').replace('D', 'N').replace('V', 'N').replace('H', 'N').replace('B', 'N').replace('X', 'N').replace('-', 'N')
     # count every k-mer in the sequence
@@ -40,35 +37,30 @@ def one_hot_k(sequence, kmers):
 
     # k-mer frequency array from the dictionary values
     k_array = np.array(list(kmer_dict.values()))
-    # normalising the array by dividing every value with the highest count value
+    # normalizing the array by dividing every value with the highest count value
     k_array = k_array / np.amax(k_array)
-
     return k_array
 
-# Generating the input and labels for train, validation and test sets
-# The encoding methods for input (x) defined earlier are now applied to the sequences, and the labels (y) are one-hot-encoded using the to_categorical function in keras_utils, thereby, converting the data into the correct format for feeding it to the deep learning models.
-# Every array is saved to reduce memory requirements.
+####################################################################################################
 
-# Encoding the sequences into k-mer counts and one-hot-encoded sequences
-
-# ENCODING SEQUENCES for the CNN model in 2 processing variations (3-mer and 6-mer)
+# ENCODING SEQUENCES for the CNN models in 2 processing variations
 # FOR CNN  |  with 3-mer
-x_train_CNN_na3 = np.array(train_na['Sequence'].apply(lambda x: one_hot_k(x, three_mers)).tolist())
+x_train_CNN_na3 = np.array(train_na['Sequence'].apply(lambda x: one_hot_k(x, tri_mers)).tolist())
 np.save('arrays/CNN/x_train_CNN_na3.npy', x_train_CNN_na3)
 
-x_train_CNN_a3 = np.array(train_a['Sequence'].apply(lambda x: one_hot_k(x, three_mers)).tolist())
-np.save('arrays/CNN/x_train_CNN_a3.npy', x_train_CNN_a3)
+# x_train_CNN_a3 = np.array(train_a['Sequence'].apply(lambda x: one_hot_k(x, tri_mers)).tolist())
+# np.save('arrays/CNN/x_train_CNN_a3.npy', x_train_CNN_a3)
 
-x_test_CNN_na3 = np.array(test_na['Sequence'].apply(lambda x: one_hot_k(x, three_mers)).tolist())
+x_test_CNN_na3 = np.array(test_na['Sequence'].apply(lambda x: one_hot_k(x, tri_mers)).tolist())
 np.save('arrays/CNN/x_test_CNN_na3.npy', x_test_CNN_na3)
 
-dataval_CNN_na3 = np.array(val_na['Sequence'].apply(lambda x: one_hot_k(x, three_mers)).tolist())
+dataval_CNN_na3 = np.array(val_na['Sequence'].apply(lambda x: one_hot_k(x, tri_mers)).tolist())
 np.save('arrays/CNN/dataval_CNN_na3.npy', dataval_CNN_na3)
 
-dataval_CNN_a3 = np.array(val_a['Sequence'].apply(lambda x: one_hot_k(x, three_mers)).tolist())
-np.save('arrays/CNN/dataval_CNN_a3.npy', dataval_CNN_a3)
+# dataval_CNN_a3 = np.array(val_a['Sequence'].apply(lambda x: one_hot_k(x, tri_mers)).tolist())
+# np.save('arrays/CNN/dataval_CNN_a3.npy', dataval_CNN_a3)
 print('3-mer complete')
-# --------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------
 # FOR CNN  |  with 6-mer
 x_train_CNN_na6 = np.array(train_na['Sequence'].apply(lambda x: one_hot_k(x, six_mers)).tolist())
 np.save('arrays/CNN/x_train_CNN_na6.npy', x_train_CNN_na6)
@@ -85,5 +77,7 @@ np.save('arrays/CNN/dataval_CNN_na6.npy', dataval_CNN_na6)
 dataval_CNN_a6 = np.array(val_a['Sequence'].apply(lambda x: one_hot_k(x, six_mers)).tolist())
 np.save('arrays/CNN/dataval_CNN_a6.npy', dataval_CNN_a6)
 print('6-mer complete')
-# --------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------
 print('CNN sequences complete')
+
+####################################################################################################
--- preprocessing_scripts/preprocessing_labels.py
+++ preprocessing_scripts/preprocessing_labels.py
@@ -1,19 +1,27 @@
+# LOADING PACKAGES
 import numpy as np
 import pandas as pd
+import keras
+import tensorflow as tf
 from tensorflow.keras.utils import to_categorical
+print('Packages loaded!')
+
+####################################################################################################
 
 # LOADING THE NON-AUGMENTED AND AUGMENTED DATASETS
-
 train_na = pd.read_csv('df_train_0.csv')
 val_na = pd.read_csv('df_val_0.csv')
 test_na = pd.read_csv('df_test_0.csv')
-
+# ------------------------------------------------
 train_a = pd.read_csv('df_train_1.csv')
 val_a = pd.read_csv('df_val_1.csv')
+# ------------------------------------------------
+print('Datasets loaded!')
 
-# One-hot-encoding the labels
+####################################################################################################
+
+# ### One-hot-encoding the labels
 # For use in the deep learning models, the labels are processed into a one-hot-encoding format. To achieve this, every unique label is first encoded to a numerical value.
-
 def get_taxon_dict(df, taxon):
     # listing all unique taxon labels
     taxon_list = list(df[taxon].unique())
@@ -25,19 +33,19 @@ def get_taxon_dict(df, taxon):
 
     return taxon_dict, taxon_dict_lookup
 
+####################################################################################################
 
-# Generating one-hot encoded train, validation and test data at the Family level
-#---------------------------------------------------------------------------------------------------------------------
-# ONE-HOT-ENCODING LABELS
+# ### Encoding the labels into a one-hot-encoded format at Family level
 taxon = 'Family'
 taxon_dict = get_taxon_dict(test_na, taxon)[0]
 
-# Associate every entry's label in the df to a number using the dictionary & one-hot encode the numerical labels
+# Associate every entry's label in the df to a number 
+#   using the dictionary & one-hot encode the numerical labels
 y_train_fam_na = to_categorical(y=train_na[taxon].map(taxon_dict).astype(np.float32))
 np.save('arrays/family/y_train_fam_na.npy', y_train_fam_na)
 
-y_train_fam_a = to_categorical(y=train_a[taxon].map(taxon_dict).astype(np.float32))
-np.save('arrays/family/y_train_fam_a.npy', y_train_fam_a)
+# y_train_fam_a = to_categorical(y=train_a[taxon].map(taxon_dict).astype(np.float32))
+# np.save('arrays/family/y_train_fam_a.npy', y_train_fam_a)
 
 y_test_fam_na = to_categorical(y=test_na[taxon].map(taxon_dict).astype(np.float32))
 np.save('arrays/family/y_test_fam_na.npy', y_test_fam_na)
@@ -45,18 +53,16 @@ np.save('arrays/family/y_test_fam_na.npy', y_test_fam_na)
 labelsval_fam_na = to_categorical(y=val_na[taxon].map(taxon_dict).astype(np.float32))
 np.save('arrays/family/labelsval_fam_na.npy', labelsval_fam_na)
 
-labelsval_fam_a = to_categorical(y=val_a[taxon].map(taxon_dict).astype(np.float32))
-np.save('arrays/family/labelsval_fam_a.npy', labelsval_fam_a)
+# labelsval_fam_a = to_categorical(y=val_a[taxon].map(taxon_dict).astype(np.float32))
+# np.save('arrays/family/labelsval_fam_a.npy', labelsval_fam_a)
 print('Family label arrays generated')
-#---------------------------------------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------
 fam_count = train_na[taxon].nunique()
 print(f'The number of unique family labels: {fam_count}')
 
-# ### At genus level
+####################################################################################################
 
-# Generating one-hot encoded train, validation and test data at the Genus level
-#---------------------------------------------------------------------------------------------------------------------
-# ONE-HOT-ENCODING LABELS
+# ### Encoding the labels into a one-hot-encoded format at Genus level
 taxon = 'Genus'
 taxon_dict = get_taxon_dict(test_na, taxon)[0]
 
@@ -75,23 +81,21 @@ np.save('arrays/genus/labelsval_gen_na.npy', labelsval_gen_na)
 labelsval_gen_a = to_categorical(y=val_a[taxon].map(taxon_dict).astype(np.float32))
 np.save('arrays/genus/labelsval_gen_a.npy', labelsval_gen_a)
 print('Genus label arrays generated')
-#---------------------------------------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------
 gen_count = train_na[taxon].nunique()
 print(f'The number of unique genus labels: {gen_count}')
 
-# ### At species level
+####################################################################################################
 
-# Generating one-hot encoded train, validation and test data at the Species level
-#---------------------------------------------------------------------------------------------------------------------
-# ONE-HOT-ENCODING LABELS
+# ### Encoding the labels into a one-hot-encoded format at Species level
 taxon = 'Species'
 taxon_dict = get_taxon_dict(test_na, taxon)[0]
 
 y_train_spe_na = to_categorical(y=train_na[taxon].map(taxon_dict).astype(np.float32))
 np.save('arrays/species/y_train_spe_na.npy', y_train_spe_na)
 
-y_train_spe_a = to_categorical(y=train_a[taxon].map(taxon_dict).astype(np.float32))
-np.save('arrays/species/y_train_spe_a.npy', y_train_spe_a)
+# y_train_spe_a = to_categorical(y=train_a[taxon].map(taxon_dict).astype(np.float32))
+# np.save('arrays/species/y_train_spe_a.npy', y_train_spe_a)
 
 y_test_spe_na = to_categorical(y=test_na[taxon].map(taxon_dict).astype(np.float32))
 np.save('arrays/species/y_test_spe_na.npy', y_test_spe_na)
@@ -99,9 +103,11 @@ np.save('arrays/species/y_test_spe_na.npy', y_test_spe_na)
 labelsval_spe_na = to_categorical(y=val_na[taxon].map(taxon_dict).astype(np.float32))
 np.save('arrays/species/labelsval_spe_na.npy', labelsval_spe_na)
 
-labelsval_spe_a = to_categorical(y=val_a[taxon].map(taxon_dict).astype(np.float32))
-np.save('arrays/species/labelsval_spe_a.npy', labelsval_spe_a)
+# labelsval_spe_a = to_categorical(y=val_a[taxon].map(taxon_dict).astype(np.float32))
+# np.save('arrays/species/labelsval_spe_a.npy', labelsval_spe_a)
 print('Species label arrays generated')
-#---------------------------------------------------------------------------------------------------------------------
-spe_count = train_na[taxon].nunique)
+# ------------------------------------------------------------------------------------
+spe_count = train_na[taxon].nunique()
 print(f'The number of unique species labels: {spe_count}')
+
+####################################################################################################
